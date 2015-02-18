8ca94110158df313265682ac9ba870d58bc7bf53
alexanderbeletsky <alexander.beletsky@gmail.com>
2015-02-18 11:09:27 +0100

    browserify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
 gulp.task('browserify', function () {
-
+	gulp.src('js/app.jsx')
+		.pipe(browserify({
+			transform: ['babelify', 'reactify']
+		}))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('build'));
 });
 
 gulp.task('default', function() {
