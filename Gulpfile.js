29b028eabfcc4a4d2e4ef9f8dd9693930854355d
alexanderbeletsky <alexander.beletsky@gmail.com>
2015-02-18 11:22:15 +0100

    gulpfile prepared

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var	connect = require('gulp-connect');
+var open = require('gulp-open');
+var watch = require('gulp-watch');
 
 gulp.task('browserify', function () {
 	gulp.src('js/app.jsx')
@@ -11,5 +14,23 @@ gulp.task('browserify', function () {
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('default', function() {
+gulp.task('connect', function () {
+	connect.server({
+		hostname: 'localhost',
+		port: 7012
+	});
+});
+
+gulp.task('watch', function () {
+	watch(['js/**/*.js', 'js/**/*.jsx'], function () {
+		gulp.start('browserify');
+	});
+});
+
+gulp.task('start:dev', ['browserify', 'connect', 'watch'],  function () {
+	var options = {
+		url: 'http://localhost:7012'
+	};
+	gulp.src('./index.html')
+		.pipe(open('', options));
 });
